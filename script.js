75acb67c6ba86fa7c203fd56214a49cfad23d8f8
Natanael <90940882+Natanaelmar7@users.noreply.github.com>
2022-03-08 12:07:19 -0300

    some changes
    Responsive

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let formulario = document.getElementById("formulario-contato")
 
+const menuList = document.getElementById("menu-list")
+    menuList.style.maxHeight = "0" 
+    
+    function showMenu(){
+       if(menuList.style.maxHeight == "0px"){
+         menuList.style.maxHeight = "120px"
+       } else{
+       menuList.style.maxHeight = "0px" 
+       }
+    }
+
 
 function chameOAbacate() {
     if (formulario.className === "form") {
